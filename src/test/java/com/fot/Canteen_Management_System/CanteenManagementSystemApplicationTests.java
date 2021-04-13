3b32ff82953510b8bc4df3f0f0f0c1bbe5486c50
SampathSandaru <54311694+SampathSandaru@users.noreply.github.com>
2021-04-13 14:20:25 +0530

    Initial commit

---
+++ src/test/java/com/fot/Canteen_Management_System/CanteenManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fot.Canteen_Management_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CanteenManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

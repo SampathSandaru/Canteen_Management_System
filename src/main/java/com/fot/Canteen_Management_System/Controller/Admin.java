d4ee16202192f048ddabeaf78a3d26cf087fd7a3
SampathSandaru <54311694+SampathSandaru@users.noreply.github.com>
2021-04-15 11:41:32 +0530

    add session

--- src/main/java/com/fot/Canteen_Management_System/Controller/Admin.java
+++ src/main/java/com/fot/Canteen_Management_System/Controller/Admin.java
@@ -1,19 +1,33 @@
 package com.fot.Canteen_Management_System.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpSession;
+import java.util.List;
 
 @Controller
 public class Admin {
 
     @RequestMapping(path = "/dash",method = RequestMethod.GET)
-    public String home(){
-        return "canteenManager/dash";
+    public String home(Model model, HttpSession session){
+        List<String> users= (List<String>) session.getAttribute("USER_SESSION");
+        model.addAttribute("users",users);
+
+        if(users==null){
+            return "redirect:/login";
+        }else{
+            return "canteenManager/dash";
+        }
     }
 
     @RequestMapping(path = "newOrder",method = RequestMethod.GET)
-    public String newOrder(){
+    public String newOrder(Model model, HttpSession session){
+        List<String> users= (List<String>) session.getAttribute("USER_SESSION");
+        model.addAttribute("users",users);
+
         return "canteenManager/NewOrder";
     }
 }
--- src/main/java/com/fot/Canteen_Management_System/Controller/UserController.java
+++ src/main/java/com/fot/Canteen_Management_System/Controller/UserController.java
@@ -5,12 +5,12 @@ import com.fot.Canteen_Management_System.Services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Controller
@@ -19,7 +19,12 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @RequestMapping(path = "/",method = RequestMethod.GET)
+    @GetMapping("/")
+    public String index(){
+        return "test";
+    }
+
+    @RequestMapping(path = "/login",method = RequestMethod.GET)
     public String login(Model model){
         User user=new User();
         model.addAttribute("user",user);
@@ -40,15 +45,36 @@ public class UserController {
     }
 
     @PostMapping("/login")
-    private String login(@ModelAttribute("user")User user, HttpServletRequest request){
+    private String login(@ModelAttribute("user")User user, HttpServletRequest request, HttpSession session){
         User newuser=userService.login(user.getEmail(),user.getPassword());
+
+        List<String> users= (List<String>) request.getSession().getAttribute("USER_SESSION");
+
         System.out.println("user : "+newuser);
         if(Objects.nonNull(newuser)){
+            if (users == null)
+            {
+                users = new ArrayList<>();
+                request.getSession().setAttribute("USER_SESSION",users);
+            }
+            users.add(newuser.getId().toString());
+            users.add(newuser.getName());
+            users.add(newuser.getEmail());
+
+            request.getSession().setAttribute("USER_SESSION",users);
+
             return "redirect:/dash";
         }else{
-            return "redirect:/?error";
+            return "redirect:/login?error";
         }
 
+    }
+
+    @GetMapping(path = "/logout")
+    public String logout(HttpServletRequest request)
+    {
+        request.getSession().invalidate();
+        return "redirect:/login";
     }
 
     @RequestMapping(path = "item",method = RequestMethod.GET)

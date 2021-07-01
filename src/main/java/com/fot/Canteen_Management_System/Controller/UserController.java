d141c64b73368e4c9c319504a25e2e8e418aae4c
Dineth Yasuru <54311243+DinethYasuru@users.noreply.github.com>
2021-07-01 16:28:32 +0530

    order dlt

--- src/main/java/com/fot/Canteen_Management_System/Controller/UserController.java
+++ src/main/java/com/fot/Canteen_Management_System/Controller/UserController.java
@@ -35,39 +35,39 @@ public class UserController {
     private SendEmailService sendEmailService;
 
     @GetMapping("/")
-    public String index(Model model){
-        model.addAttribute("items",itemService.getuserItem());
+    public String index(Model model) {
+        model.addAttribute("items", itemService.getuserItem());
         return "index";
     }
 
-    @RequestMapping(path = "/loginpage",method = RequestMethod.GET)
-    public String login(Model model){
-        User user=new User();
-        model.addAttribute("user",user);
+    @RequestMapping(path = "/loginpage", method = RequestMethod.GET)
+    public String login(Model model) {
+        User user = new User();
+        model.addAttribute("user", user);
         return "loginNew";
     }
 
-    @RequestMapping(path = "/register",method = RequestMethod.GET)
-    public String register(Model model){
-        User user=new User();
-        model.addAttribute("user",user);
+    @RequestMapping(path = "/register", method = RequestMethod.GET)
+    public String register(Model model) {
+        User user = new User();
+        model.addAttribute("user", user);
         return "register1";
     }
 
-    @RequestMapping(path = "/reg_user",method = RequestMethod.POST)
-    public String reg_new_user(@ModelAttribute("User") User user){
+    @RequestMapping(path = "/reg_user", method = RequestMethod.POST)
+    public String reg_new_user(@ModelAttribute("User") User user) {
         userService.save(user);
         return "redirect:/loginpage";
     }
 
     @PostMapping("/login")
-    private String login(@ModelAttribute("user")User user, HttpServletRequest request, HttpSession session){
-        User newuser=userService.login(user.getEmail(),user.getPassword());
+    private String login(@ModelAttribute("user") User user, HttpServletRequest request, HttpSession session) {
+        User newuser = userService.login(user.getEmail(), user.getPassword());
 
-        List<String> users= (List<String>) request.getSession().getAttribute("USER_SESSION");
+        List<String> users = (List<String>) request.getSession().getAttribute("USER_SESSION");
 
-        System.out.println("user : "+newuser);
-        if(Objects.nonNull(newuser)) {
+        System.out.println("user : " + newuser);
+        if (Objects.nonNull(newuser)) {
             if (users == null) {
                 users = new ArrayList<>();
                 request.getSession().setAttribute("USER_SESSION", users);
@@ -83,190 +83,197 @@ public class UserController {
                 return "redirect:/dash";
             } else if (newuser.getRole().equals("user") && newuser.getApprove() == 1) {
                 return "redirect:/item";
-            }else if(newuser.getRole().equals("admin") && newuser.getApprove()==1){
+            } else if (newuser.getRole().equals("admin") && newuser.getApprove() == 1) {
                 return "redirect:/admin";
-            }else{
+            } else {
                 return "redirect:/loginpage?notapproved";
             }
 
-        }else{
+        } else {
             return "redirect:/loginpage?incorrectDetail";
         }
 
     }
 
     @GetMapping(path = "/logout")
-    public String logout(HttpServletRequest request)
-    {
+    public String logout(HttpServletRequest request) {
         request.getSession().invalidate();
         return "redirect:/";
     }
 
-    @RequestMapping(path = "item",method = RequestMethod.GET)
-    public String home(Model model,HttpSession session){
-        List<String> users= (List<String>) session.getAttribute("USER_SESSION");
-        OrderItem orderItem=new OrderItem();
+    @RequestMapping(path = "item", method = RequestMethod.GET)
+    public String home(Model model, HttpSession session) {
+        List<String> users = (List<String>) session.getAttribute("USER_SESSION");
+        OrderItem orderItem = new OrderItem();
 
-        if(users==null){
+        if (users == null) {
             return "redirect:/loginpage";
-        }else{
-            model.addAttribute("items",itemService.getuserItem());
-            model.addAttribute("users",users);
-            model.addAttribute("order_item",orderItem);
+        } else {
+            model.addAttribute("items", itemService.getuserItem());
+            model.addAttribute("users", users);
+            model.addAttribute("order_item", orderItem);
             return "User/item";
         }
     }
 
-    @RequestMapping(path = "/orderitem",method = RequestMethod.POST)
-    public String orderitem(@ModelAttribute("OrderItem")OrderItem orderItem, HttpSession session){
-        List<String> users= (List<String>) session.getAttribute("USER_SESSION");
+    @RequestMapping(path = "/orderitem", method = RequestMethod.POST)
+    public String orderitem(@ModelAttribute("OrderItem") OrderItem orderItem, HttpSession session) {
+        List<String> users = (List<String>) session.getAttribute("USER_SESSION");
         orderItem.setU_id(Integer.parseInt(users.get(0)));
         LocalDate date = LocalDate.now();
 
-        float tot=orderItem.getQuantity()*orderItem.getPrice();
+        float tot = orderItem.getQuantity() * orderItem.getPrice();
         orderItem.setPrice(tot);
         orderItem.setOrder_time(date);
-        itemService.reduce(orderItem.getQuantity(),orderItem.getItem_id());
+        itemService.reduce(orderItem.getQuantity(), orderItem.getItem_id());
 
         orderItemService.save(orderItem);
         return "redirect:/item?ordersuccess";
     }
 
 
-    @RequestMapping(path = "OrderHistory",method = RequestMethod.GET)
-    public String OrderHistory(Model model,HttpSession session){
-        List<String> users= (List<String>) session.getAttribute("USER_SESSION");
+    @RequestMapping(path = "OrderHistory", method = RequestMethod.GET)
+    public String OrderHistory(Model model, HttpSession session) {
+        List<String> users = (List<String>) session.getAttribute("USER_SESSION");
 
-        Integer user_id=Integer.parseInt(users.get(0));
+        Integer user_id = Integer.parseInt(users.get(0));
 
-        List<OrderItemDto> user_Order_item= orderItemRepository.getorderHistory(user_id); //join query
+        List<OrderItemDto> user_Order_item = orderItemRepository.getorderHistory(user_id); //join query
 
-        if(users==null){
+        if (users == null) {
             return "redirect:/loginpage";
-        }else{
+        } else {
 
-            model.addAttribute("user_Order_item",user_Order_item);
-            model.addAttribute("items",itemService.getuserItem());
-            model.addAttribute("users",users);
+            model.addAttribute("user_Order_item", user_Order_item);
+            model.addAttribute("items", itemService.getuserItem());
+            model.addAttribute("users", users);
             return "User/OrderHistory";
 
         }
     }
 
 
-
     @GetMapping("/profile")
-    public String profile(HttpSession session,Model model){
-        List<String> user= (List<String>) session.getAttribute("USER_SESSION");
+    public String profile(HttpSession session, Model model) {
+        List<String> user = (List<String>) session.getAttribute("USER_SESSION");
 
-        if(user==null){
+        if (user == null) {
             return "redirect:/loginpage";
-        }else {
-            Integer userId=Integer.parseInt(user.get(0));
-            User user1=userService.getUserId(userId);
-            model.addAttribute("pwdchagedate",userService.pwdchnagedate(userId));
-            model.addAttribute("users",user);
-            model.addAttribute("users_obj",user1);
+        } else {
+            Integer userId = Integer.parseInt(user.get(0));
+            User user1 = userService.getUserId(userId);
+            model.addAttribute("pwdchagedate", userService.pwdchnagedate(userId));
+            model.addAttribute("users", user);
+            model.addAttribute("users_obj", user1);
             return "User/user_profile";
         }
     }
 
     @GetMapping("userdash")
-    public String userdash(Model model,HttpSession session){
-        List<String> users= (List<String>) session.getAttribute("USER_SESSION");
+    public String userdash(Model model, HttpSession session) {
+        List<String> users = (List<String>) session.getAttribute("USER_SESSION");
 
-        if(users==null){
+        if (users == null) {
             return "redirect:/loginpage";
-        }else{
-            model.addAttribute("users",users);
+        } else {
+            model.addAttribute("users", users);
             return "User/user_dash";
         }
     }
 
 
-    @RequestMapping(path = "/update_profile",method = RequestMethod.POST)
-    public String update_profile(@ModelAttribute("User")User user,HttpSession session){
-        List<String> user1= (List<String>) session.getAttribute("USER_SESSION");
+    @RequestMapping(path = "/update_profile", method = RequestMethod.POST)
+    public String update_profile(@ModelAttribute("User") User user, HttpSession session) {
+        List<String> user1 = (List<String>) session.getAttribute("USER_SESSION");
 
-        Integer userId=Integer.parseInt(user1.get(0));
-        if(userService.updateProfile(userId,user)){
+        Integer userId = Integer.parseInt(user1.get(0));
+        if (userService.updateProfile(userId, user)) {
             return "redirect:/profile?success";
-        }else{
+        } else {
             return "redirect:/profile?error";
         }
     }
 
-    @RequestMapping(path = "/chang_pwd",method = RequestMethod.POST)
-    public String chang_pwd(HttpSession session,@RequestParam("current_pwd")String current_pwd,@RequestParam("new_pwd")String new_pwd,@RequestParam("confim_pwd")String confirm_pwd){
-        List<String> user1= (List<String>) session.getAttribute("USER_SESSION");
+    @RequestMapping(path = "/chang_pwd", method = RequestMethod.POST)
+    public String chang_pwd(HttpSession session, @RequestParam("current_pwd") String current_pwd, @RequestParam("new_pwd") String new_pwd, @RequestParam("confim_pwd") String confirm_pwd) {
+        List<String> user1 = (List<String>) session.getAttribute("USER_SESSION");
 
-        Integer id=Integer.parseInt(user1.get(0));
-       if(new_pwd.equals(confirm_pwd)){
-           if(userService.chang_password(current_pwd,id,new_pwd)){
-               return "redirect:/profile?chngpwdsuccess";
-           }else{
-               return "redirect:/profile?incorect_current_pwd";
-           }
-       }else{
-           return "redirect:/profile?dosentmachpwd";
-       }
+        Integer id = Integer.parseInt(user1.get(0));
+        if (new_pwd.equals(confirm_pwd)) {
+            if (userService.chang_password(current_pwd, id, new_pwd)) {
+                return "redirect:/profile?chngpwdsuccess";
+            } else {
+                return "redirect:/profile?incorect_current_pwd";
+            }
+        } else {
+            return "redirect:/profile?dosentmachpwd";
+        }
+    }
+
+    @RequestMapping(path = "/delete_order", method = RequestMethod.POST)
+    public String delete_order(@RequestParam("id") Integer id) {
+
+        if (orderItemService.deleteOrdere(id)) {
+            return "redirect:/OrderHistory?success";
+        } else {
+            return "redirect:/OrderHistory?error";
+        }
     }
 
 
-
     @GetMapping(path = "/email")
-    public String email(){
+    public String email() {
         return "email1";
     }
 
     @GetMapping(path = "/reset_code")
-    public String reset_code(){
+    public String reset_code() {
         return "reset_code1";
     }
 
     @GetMapping(path = "/reset_pwd")
-    public String reset_pwd(){
+    public String reset_pwd() {
         return "reset_pwd1";
     }
 
-    @RequestMapping(path = "/forget_pwd_email",method = RequestMethod.POST)
-    public String codepage(HttpSession session,@RequestParam("email")String email){
-        User user=userService.forget_pwd(email);
-        if(Objects.nonNull(user)){
-            int max=9999;
-            int min=1000;
-            int code = (int) ((Math.random()*(max-min))+min);
-            String s_cod=String.valueOf(code);
-            session.setAttribute("reset_code",s_cod);
-            session.setAttribute("id",user.getId());
-            sendEmailService.sendEmial(email,"Your Password Reset Code : "+code,"Reset Code");
+    @RequestMapping(path = "/forget_pwd_email", method = RequestMethod.POST)
+    public String codepage(HttpSession session, @RequestParam("email") String email) {
+        User user = userService.forget_pwd(email);
+        if (Objects.nonNull(user)) {
+            int max = 9999;
+            int min = 1000;
+            int code = (int) ((Math.random() * (max - min)) + min);
+            String s_cod = String.valueOf(code);
+            session.setAttribute("reset_code", s_cod);
+            session.setAttribute("id", user.getId());
+            sendEmailService.sendEmial(email, "Your Password Reset Code : " + code, "Reset Code");
             return "redirect:/reset_code?success";
-        }else{
+        } else {
             return "redirect:/email?error";
         }
     }
 
-    @RequestMapping(path = "/pwd_reset_code",method = RequestMethod.POST)
-    public String pwd_reset_code(HttpSession session,@RequestParam("resetcode")String code){
-        if(session.getAttribute("reset_code").equals(code)){
-            System.out.println("OK "+session.getAttribute("reset_code"));
+    @RequestMapping(path = "/pwd_reset_code", method = RequestMethod.POST)
+    public String pwd_reset_code(HttpSession session, @RequestParam("resetcode") String code) {
+        if (session.getAttribute("reset_code").equals(code)) {
+            System.out.println("OK " + session.getAttribute("reset_code"));
             return "redirect:/reset_pwd";
-        }else{
-            System.out.println("erro "+session.getAttribute("reset_code"));
+        } else {
+            System.out.println("erro " + session.getAttribute("reset_code"));
             return "redirect:/reset_code?error";
         }
     }
 
-    @RequestMapping(path = "/reset_pwd",method = RequestMethod.POST)
-    public String reset_pwd(HttpSession session,@RequestParam("confirm_pwd")String confirm_pwd,@RequestParam("pwd")String pwd){
-        Integer id= (Integer) session.getAttribute("id");
-        if(pwd.equals(confirm_pwd)){
-            if(userService.reset_password(pwd,id)){
+    @RequestMapping(path = "/reset_pwd", method = RequestMethod.POST)
+    public String reset_pwd(HttpSession session, @RequestParam("confirm_pwd") String confirm_pwd, @RequestParam("pwd") String pwd) {
+        Integer id = (Integer) session.getAttribute("id");
+        if (pwd.equals(confirm_pwd)) {
+            if (userService.reset_password(pwd, id)) {
                 return "redirect:/loginpage";
-            }else{
+            } else {
                 return "redirect:/reset_code";
             }
-        }else
+        } else
             return "redirect:/reset_pwd?error_matchPwd";
     }
 }
